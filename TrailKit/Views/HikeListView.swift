c7b930f6446f861b786b1250505369ce63510069
krishpranav <krisna.pranav@gmail.com>
2026-03-30 12:55:21 +0530

    TrailKit: HikeListView

---
+++ TrailKit/Views/HikeListView.swift
@@ -0,0 +1,22 @@
+//
+//  HikeListView.swift
+//  TrailKit
+//
+//  Created by Krisna Pranav on 30/03/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HikeListView: View {
+    
+    var body: some View {
+    }
+}
+
+
+
+
+#Preview {
+    HikeListView()
+}
