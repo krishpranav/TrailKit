6606d48d02d17fd3995c2ccf3f64b3f99cd0e240
krishpranav <krisna.pranav@gmail.com>
2026-03-30 12:21:59 +0530

    base project

---
+++ TrailKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrailKit
+//
+//  Created by Krisna Pranav on 30/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrailKit/TrailKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailKitApp.swift
+//  TrailKit
+//
+//  Created by Krisna Pranav on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
