6b9020d91cd0b5bc6bac5617257767b0fcfb07bd
krishpranav <krisna.pranav@gmail.com>
2026-03-30 12:53:54 +0530

    ContentView: implement tab view

--- TrailKit/ContentView.swift
+++ TrailKit/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var store = HikeStore()
+    @State private var selectedTab: AppTab = .hikes
+
+    enum AppTab {
+        case hikes, charts, explore, settings
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab) {
+            Tab("Hikes", systemImage: "figure.hiking",
+                value: AppTab.hikes) {
+            }
+
+            Tab("Charts", systemImage: "chart.xyaxis.line",
+                value: AppTab.hikes) {
+            }
+
+            Tab("Explore", systemImage: "safari",
+                value: AppTab.hikes) {
+            }
+
+            Tab("Settings", systemImage: "gearshape",
+                value: AppTab.hikes) {
+            }
         }
-        .padding()
     }
 }
 
